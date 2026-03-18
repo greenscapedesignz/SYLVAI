16e6116ce1ff277f08934184a621fcfb9ce7b206
Anirban Pal <greenscapedesignz@gmail.com>
2026-03-18 16:00:51 +0530

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  // Replace 'SYLVAI' with your exact GitHub repository name
   base: "/SYLVAI/",
 });
